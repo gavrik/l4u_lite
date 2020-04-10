74bbc958310578581a00599097007bcb6c1f9e31
Mike Gavrylov <gavrikster@gmail.com>
2020-04-10 23:43:50 +0300

    rest support functions

--- api/rest.go
+++ api/rest.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"lib"
+
 	"github.com/gin-gonic/gin"
 )
 
+// AuthTokenKey -
+const AuthTokenKey = "AdminToken"
+
 // REST - rest
-type REST interface{
+type REST interface {
 	Post(c *gin.Context)
 	Get(c *gin.Context)
 	Delete(c *gin.Context)
@@ -14,5 +20,33 @@ type REST interface{
 // NewLink - REST implementation for link section
 func NewLink() REST {
 	rest := new(LinkImplementation)
+	rest.Db = lib.OpenDB(config.DatabasePath)
 	return rest
+}
+
+// RESTErrorFunc -
+func RESTErrorFunc(errNo int, errMsg string) RESTError {
+	return RESTError{errNo, errMsg}
+}
+
+// GetAuthorizationToken -
+func GetAuthorizationToken(c *gin.Context) string {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader != "" {
+		return authHeader[6:len(authHeader)]
+	}
+	return ""
+}
+
+// TokenAuthorization -
+func TokenAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenHash := GetAuthorizationToken(c)
+		fmt.Println(tokenHash)
+		if token, ok := TokenCache[tokenHash]; ok {
+			c.Set(AuthTokenKey, token)
+		} else {
+			c.Set(AuthTokenKey, AdminToken{})
+		}
+	}
 }
